5e1a6b35e4648ea28c4a32caa242af49764d0c07
gdesousa <gedesousa@gmail.com>
2024-03-06 20:28:25 +0100

    No perder la seleccion entre pantallas

--- chatbot/chatbot.UI/Controllers/HomeController.cs
+++ chatbot/chatbot.UI/Controllers/HomeController.cs
@@ -9,6 +9,8 @@ namespace chatbot.UI.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly ChatbotApplicationService _chatbotApplicationService;
+        private List<string>  themes = new List<string> { "Python", "C#", "JavaScript", "Java", "Angular" };
+
 
         public HomeController(ILogger<HomeController> logger, ChatbotApplicationService chatbotApplicationService)
         {
@@ -19,16 +21,22 @@ namespace chatbot.UI.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.Themes = themes;
+
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Index(string message, string theme)
+        public async Task<IActionResult> Index(ChatbotViewModel model)
         {
-            var botMessage = await _chatbotApplicationService.GenerateResponseAsync(message, theme);
-            
+            var botMessage = await _chatbotApplicationService.GenerateResponseAsync(model.Message, model.Theme);
+
+            ViewBag.Themes = themes;
+            ViewBag.Theme = model.Theme; // Almacena el tema seleccionado en el ViewBag
+
             return View("Index", botMessage);
         }
+
 
         public IActionResult Privacy()
         {
---
+++ chatbot/chatbot.UI/Models/ChatbotViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace chatbot.UI.Models
+{    public class ChatbotViewModel
+    {
+        public string Message { get; set; } = string.Empty;
+        public string Theme { get; set; } = string.Empty;
+
+    }
+
+}
