8ff1447c6c124aade3f57a278da9d95a6f4f64e0
Guly de Sousa <gdesousa@gmail.com>
2024-04-27 22:21:43 +0200

    C# Fundamentals

---
+++ cs12dotnet8/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Animal
+{
+
+    public string Name { get; set; }
+    public DateTime Born { get; set; }
+    public int Legs { get; set; }
+
+}
+
+public class Cat: Animal
+{
+    public bool  IsDomestic { get; set; }
+}
+
+public class Spider: Animal
+{
+    public bool IsPoisonous { get; set; }
+}
+
