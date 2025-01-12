752159f52df646393ed000453a6485a54621956a
Guly Mary de Sousa Garrido <gdesousa@gmail.com>
2025-01-12 20:30:06 +0100

    Configure the HTTP request pipeline.

--- PracticalApps/Northwind.Web/Program.cs
+++ PracticalApps/Northwind.Web/Program.cs
@@ -1,25 +1,37 @@
+//Para crear el host del website
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+//builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    //app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
 
-app.UseAuthorization();
+app.MapGet("/", () => "Hello World!");
 
-app.MapRazorPages();
 
-app.Run();
+
+
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapRazorPages();
+
+
+//Inicia el web server, hospeda el website, espera por request
+app.Run(); //thread-blocking call
+
+Console.WriteLine("Se ejecuta cuando se para el servidor");
+
